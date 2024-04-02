818076830800a2787d39b29cbb46338f98fe5c09
Naoki Haba <naosprintrunner6385@gmail.com>
2024-04-02 22:26:29 +0900

    Switch to productStore for product data

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <script setup>
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
-import products from "@/data/products.json";
+import { useProductStore } from "@/stores/ProductStore"
+
+const productStore = useProductStore()
+productStore.fill()
+
+useProductStore()
 </script>
 
 <template>
   <div class="container">
-    <TheHeader />
+    <TheHeader/>
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
-        v-for="product in products"
-        :key="product.name"
-        :product="product"
+          v-for="product in productStore.products"
+          :key="product.name"
+          :product="product"
       />
     </ul>
   </div>
--- src/main.js
+++ src/main.js
@@ -9,11 +9,13 @@ import "./assets/main.pcss";
 import AppButton from "./components/AppButton.vue";
 import AppCountInput from "./components/AppCountInput.vue";
 import AppModalOverlay from "./components/AppModalOverlay.vue";
+import { createPinia } from "pinia"
 
 // Init App
 createApp(App)
-  .use(FontAwesomePlugin)
-  .component("AppButton", AppButton)
-  .component("AppCountInput", AppCountInput)
-  .component("AppModalOverlay", AppModalOverlay)
-  .mount("#app");
+    .use(createPinia())
+    .use(FontAwesomePlugin)
+    .component("AppButton", AppButton)
+    .component("AppCountInput", AppCountInput)
+    .component("AppModalOverlay", AppModalOverlay)
+    .mount("#app");
